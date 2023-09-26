525b4010f1dee32725642ebeb18f19bfe08f8c88
Austin Slater <austinslater58@gmail.com>
2023-09-25 20:27:28 -0500

    script now accepts multiple word cities

--- sortCityTemps.sh
+++ sortCityTemps.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+# List of cities (including those with multiple words)
+cities=("New York" "Los Angeles" "Hong Kong" "San Fernando del Valle de Catamarca")
 
-# Define an array of cities
-cities=("Paris" "Tokyo" "London")
+# Create an empty file to store the weather data
+> weather_data.txt
 
-# Create an empty file to store temperatures
-> temperatures.txt
-
-# Loop through the cities
+# Iterate through the cities
 for city in "${cities[@]}"
 do
-  # Pause for 1 second
-  sleep 1
-  
-  # Fetch weather information and simplify the output
-  weather_output=$(./weather.sh -s "$city")
-  
-  # Remove extra symbols like + and °F
-  simplified_output=$(echo "$weather_output" | sed 's/+//' | sed 's/°F//')
-  
-  # Append the simplified weather output to temperatures.txt
-  echo "$simplified_output" >> temperatures.txt
+    # Pause for 2 seconds between requests (to avoid overwhelming the API)
+    sleep 2
+
+    # Fetch weather data using the modified weather script
+    weather_output=$(./weather.sh -s "$city")
+
+    # Extract temperature and wind speed from the weather output
+    temperature=$(echo "$weather_output" | grep -oE '[+-]?[0-9]+[°][CF]' | head -n1)
+    wind_speed=$(echo "$weather_output" | grep -oE '[0-9]+[kKmM/.]+[hH]' | head -n1)
+
+    # Append the data to the weather_data.txt file
+    echo "$city: Temperature $temperature, Wind Speed $wind_speed" >> weather_data.txt
 done
 
-# Sort temperatures in descending order and save the sorted output
-sort -k2 -r temperatures.txt > sorted_temperatures.txt
+# Sort the data based on wind speed in descending order
+sort -t':' -k5nr -o sorted_weather_data.txt weather_data.txt
+
+# Display the sorted data
+cat sorted_weather_data.txt
